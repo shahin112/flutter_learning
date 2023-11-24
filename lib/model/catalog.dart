57199826c02228f022861c57c9f88fccd5e25d29
shahinahmed <shahiahmed973@gmail.com>
2023-11-24 15:53:19 +0300

    animated

--- lib/model/catalog.dart
+++ lib/model/catalog.dart
@@ -1,15 +1,12 @@
- 
-
 class CatalogModel {
-  List<Item> items=[];
- 
+  static List<Item> items = [];
 }
 
 class Item {
   final String id;
   final String name;
   final String desc;
-  final num price;
+  final String price;
   final String color;
   final String image;
 
@@ -17,29 +14,28 @@ class Item {
     required this.id,
     required this.name,
     required this.desc,
-    required this.price, 
+    required this.price,
     required this.color,
     required this.image,
   });
 
   factory Item.fromMap(Map<String, dynamic> map) {
     return Item(
-      id: map["id"],
+      id: map["id"].toString(),
       name: map["name"],
-      desc: map["desc"],
-      price: map["price"],  
-      color: map["color"],
-      image: map["image"],
+      desc: map["desc"] ,
+      price: map["price"].toString(),
+      color: map["color"] ,
+      image: map["image"] ,
     );
   }
 
-  toMap() => {
+  Map<String, dynamic> toMap() => {
     "id": id,
     "name": name,
     "desc": desc,
     "price": price,
     "color": color,
-    "image": image
+    "image": image,
   };
 }
-
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -13,4 +13,8 @@ class MyTheme {
           )));
 
   static ThemeData dartTheme(BuildContext context) => ThemeData(brightness: Brightness.dark);
+
+  //Color
+  static Color creamColor=Color(0xfff5f5f5);
+  static Color darkBluishColor=Color(0xff403b58);
 }
